971a14a63f8318387b5cc382ef076daae8b960cc
Matthew Hemmings <discount.yoyos@gmail.com>
2017-07-20 21:04:24 -0700

    Added MK setup script

---
+++ setup_mk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install -y curl wget software-properties-common
+https://www.virtualbox.org/download/oracle_vbox_2016.asc
+sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' >> /etc/apt/sources.list"
+sudo apt-get update
+sudo apt-key add ./oracle_vbox_2016.asc
+sudo apt-get install -y virtualbox-5.1
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.20.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+
