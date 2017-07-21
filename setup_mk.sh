d58187b6682ebaa093578b8ec6d23c45cb89693a
MHemmings <discount.yoyos@gmail.com>
2017-07-20 21:10:40 -0700

    Corrected order for adding repo and key to sources

--- setup_mk.sh
+++ setup_mk.sh
@@ -3,8 +3,8 @@
 sudo apt-get install -y curl wget software-properties-common
 wget https://www.virtualbox.org/download/oracle_vbox_2016.asc
 sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' >> /etc/apt/sources.list"
-sudo apt-get update
 sudo apt-key add ./oracle_vbox_2016.asc
+sudo apt-get update
 sudo apt-get install -y virtualbox-5.1
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod +x kubectl && sudo mv kubectl /usr/local/bin/
