1c9dddae7d3656fa496c0fdd93498d1d600a41cc
Matthew Hemmings <discount.yoyos@gmail.com>
2017-07-24 11:10:09 -0700

    Created setup folder for files for helloworld minikube deployment

--- minikube/setup_mk.sh
+++ minikube/setup_mk.sh
@@ -7,8 +7,12 @@ sudo apt-key add ./oracle_vbox_2016.asc
 sudo apt-get update
 sudo apt-get install -y virtualbox-5.1
 
+sudo ifconfig vboxnet0 up
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.20.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
-
+mkdir ./hellonode
+cd hellonode
+wget 
