cbe5d859c0bf57da7ece9b7e894c839303cb1820
Matthew Hemmings <discount.yoyos@gmail.com>
2017-07-27 10:12:32 -0700

    Fleshed out head script

--- minikube/kubecluster/starthead.sh
+++ minikube/kubecluster/starthead.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+#Ensure kubeadm is ready
+sudo kubeadm reset
+#Ensure kubelet is in a waiting state
+sudo service kubelet restart
+#init kubernetes
+sudo kubeadm init
+
+#Copy and take ownership of current config
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#create and apply flannel overlay network
+kubectl create -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
