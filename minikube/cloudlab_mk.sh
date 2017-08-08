64b28c9d7fafddf21cd6f5631990ff65be335cb4
MHemmings <discount.yoyos@gmail.com>
2017-08-08 12:53:53 -0700

    Update cloudlab_mk.sh

--- minikube/cloudlab_mk.sh
+++ minikube/cloudlab_mk.sh
@@ -13,33 +13,33 @@ minikube start
 sudo ifconfig vboxnet0 up
 
 #Set docker context to minikube.  To teardown: eval $(minikube docker-env -u)
-eval $(minikube docker-env)
+#eval $(minikube docker-env)
 
 #build docker container in minikube
-sudo docker build -f $(pwd)/hellonode/Dockerfile -t hello-node:v1 .
+#sudo docker build -f $(pwd)/hellonode/Dockerfile -t hello-node:v1 .
 
 #create minikube deployment
-kubectl run hello-node --image=hello-node:v1 --port=8080
+#kubectl run hello-node --image=hello-node:v1 --port=8080
 
 #create a service
-kubectl expose deployment hello-node --type=LoadBalancer
+#kubectl expose deployment hello-node --type=LoadBalancer
 
 sudo apt-get install nginx -y
-sudo $(pwd)/hellonode/makeConfig.sh $(minikube service hello-node --url)
-sudo nginx -s reload
+#sudo $(pwd)/hellonode/makeConfig.sh $(minikube service hello-node --url)
+#sudo nginx -s reload
 
-printf "\nSetup complete:\n"
-printf "To view contents, launch browser:\n"
-printf "minikube service hello-node\n"
-printf "\n"
-printf "To update, change contents of server.js, rebuild docker container and assign new image to service (example):\n"
-printf "docker build -t hello-node:v2 ."
-printf "kubectl set image deployment/hello-node hello-node=hello-node:v2\n"
-printf "\n"
-printf "To tear down:\n"
-printf "eval \$(minikube docker-env -u)\n"
-printf "kubectl delete service hello-node\n"
-printf "kubectl delete deployment hello-node\n"
-printf "minikube stop"
+#printf "\nSetup complete:\n"
+#printf "To view contents, launch browser:\n"
+#printf "minikube service hello-node\n"
+#printf "\n"
+#printf "To update, change contents of server.js, rebuild docker container and assign new image to service (example):\n"
+#printf "docker build -t hello-node:v2 ."
+#printf "kubectl set image deployment/hello-node hello-node=hello-node:v2\n"
+#printf "\n"
+#printf "To tear down:\n"
+#printf "eval \$(minikube docker-env -u)\n"
+#printf "kubectl delete service hello-node\n"
+#printf "kubectl delete deployment hello-node\n"
+#printf "minikube stop"
 
  
