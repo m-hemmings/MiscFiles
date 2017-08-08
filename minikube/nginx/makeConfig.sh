590c0ec77ad50961188077f65efd299e3d93cf09
MHemmings <discount.yoyos@gmail.com>
2017-08-08 12:42:04 -0700

    Update makeConfig.sh

--- minikube/nginx/makeConfig.sh
+++ minikube/nginx/makeConfig.sh
@@ -13,20 +13,24 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+url=$(awk -F/ '{print $NF}' | cut -d ":" -f1)
+port=$(awk -F/ '{print $NF}' | cut -d ":" -f2)
+
 echo "# nginx base config file to redirect to minikube location
 # To be placed in /etc/nginx/sites-enabled/
 
 server {
   listen 80;
   location /minikube/ {
-    proxy_pass $1;
+    proxy_pass http://$url:$port
+    $1;
     proxy_http_version 1.1;
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection "upgrade";      
       proxy_pass_request_headers on;
   }
   location / {
-    proxy_pass $(minikube dashboard --url);
+    proxy_pass http://$url:30000;
   }
 }" > /etc/nginx/sites-enabled/minikube.conf
 
