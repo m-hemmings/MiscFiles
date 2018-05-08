e3231f57f7dcb4d37dfa9e78acfc6e10f4e6ab7d
Matt Hemmings <discount.yoyos@gmail.com>
2018-05-08 09:59:16 -0700

    Add untested contents of setup

---
+++ scripts/setup_node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+USERID=id -u
+if [ "$USERID" -ne 0 ]
+  then  echo "Script requires to be run as root"
+  exit
+fi
+
+apt-get update && apt-get install -y apt-transport-https -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat << EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update 
+apt-get install docker.io kubelet kubeadm kubectl kubernetes-cni -y
+SECRET = curl('https://head.sundewproject.org/get_secret')
+kubeadm join 142.104.71.105:6443 --token $SECRET --discovery-token-ca-cert-hash sha256:4d4d2122e5fff8cb29fc6c628b4ac0a4ceeea6fbb8243b95d1d37a1a05190730
