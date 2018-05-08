9108e5fe4acff5ba65ac3a469cb1694c0fa6939f
Matt Hemmings <discount.yoyos@gmail.com>
2018-05-08 10:52:33 -0700

    Correct server port for query

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -12,5 +12,5 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update 
 apt-get install docker.io kubelet kubeadm kubectl kubernetes-cni -y
-SECRET = curl('https://head.sundewproject.org/get_secret')
+SECRET = curl('https://head.sundewproject.org:8181/get_secret')
 kubeadm join 142.104.71.105:6443 --token $SECRET --discovery-token-ca-cert-hash sha256:4d4d2122e5fff8cb29fc6c628b4ac0a4ceeea6fbb8243b95d1d37a1a05190730
